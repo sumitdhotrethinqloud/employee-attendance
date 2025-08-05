a52c3db5825f0ae1ec699558a6e68d6f34f75e0c
sumitdhotrethinqloud <sumit.dhotre@thinqloud.com>
2025-08-05 13:56:42 +0530

    Add routing for settings page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import ConfigUI from "./configView";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Router>
     <Routes>
+      {/* Main attendance app */}
       <Route path="/" element={<App />} />
+
+      {/* Settings page */}
       <Route path="/settings" element={<ConfigUI />} />
     </Routes>
   </Router>
